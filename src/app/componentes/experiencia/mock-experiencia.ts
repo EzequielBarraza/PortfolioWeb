47495c6f497ebb10fad704c43185a0f7d414b724
Ezequiel Barraza <ezequielbarraza@live.com>
2022-03-08 16:39:16 -0300

    Cambios en experiencia.json y educación.json

--- src/app/componentes/experiencia/mock-experiencia.ts
+++ src/app/componentes/experiencia/mock-experiencia.ts
@@ -1,19 +1,19 @@
 import {Experiencia} from "./Experiencia"
 
 export const EXPERIENCIA : Experiencia [] = [{
-    id: 2,
+    id: 1,
     cargo:'Secretario de Recursos Humanos',
     institucion:'Instituto Miguel de Cervantes Saavedra',
     periodo: 'Periodo: 2019 - Actualmente'
     },
 {
-id: 1,
+id: 2,
 cargo:'Secretario Nivel Secundario',
 institucion:'Instituto Miguel de Cervantes Saavedra',
 periodo:'Periodo: 2013 - Actualmente'
 },
 {
-    id: 2,
+    id: 3,
     cargo:'Profesor de Ed. Física',
     institucion:'Instituto Miguel de Cervantes Saavedra',
     periodo: 'Periodo: 2010 - Actualmente'
