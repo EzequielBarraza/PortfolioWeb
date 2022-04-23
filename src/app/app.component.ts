c4627059ed7ca0dcf52cba5a813fa664f288a87f
Ezequiel Barraza <ezequielbarraza@live.com>
2022-04-22 23:36:50 -0300

    Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {AuthService} from 'src/app/servicios/auth.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PortfolioWeb';
+  isUserLogged: boolean = false;
+  constructor( private authService: AuthService) {}
+  ngOnInit(): void {
+    this.isUserLogged = this.authService.isUserLogged();
+  }
 }
